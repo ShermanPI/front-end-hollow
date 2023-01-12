3a461fa58ad374c4d579182589044ee1a52045df
Sherman <shermanperez0@gmail.com>
2023-01-12 01:04:26 -0400

    burger btn svg

--- UI/burger-menu.js
+++ UI/burger-menu.js
@@ -1,4 +1,5 @@
-const d = document
+const d = document,
+    root = document.documentElement;
 
 export function burgerMenu(btnSelector, menuSelector){
     const $menuBtn = d.querySelector(btnSelector),
@@ -6,13 +7,16 @@ export function burgerMenu(btnSelector, menuSelector){
         
     d.addEventListener("click", (e)=>{
 
-
-        if(e.target == $menuBtn){
+        if(e.path[1] == $menuBtn){
             if($menu.classList.contains("hide-menu")){
                 $menu.classList.remove("hide-menu");
-                
+                $menuBtn.classList.add('open');
+                root.style.setProperty('--header-color', "#0f0f1b");
             }else{
-                $menu.classList.add("hide-menu")
+                $menu.classList.add("hide-menu");
+                $menuBtn.classList.remove('open');
+                root.style.setProperty('--header-color', "black");
+
             }
             
         }
