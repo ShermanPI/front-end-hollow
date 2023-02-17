7d2b8b43c971665d0b055c5675770fb3fd6f398a
Estarlin Valerio <valerioestarlin4tog@gmail.com>
2023-02-17 16:50:56 -0400

    change profile pic

--- UI/edit_profile.js
+++ UI/edit_profile.js
@@ -4,11 +4,39 @@ export function editProfile(){
     const $editPfpBtn = d.querySelector(".change-pfp-text"),
         $editPgpBtnIcon = d.querySelector(".edit-profile-icon"),
         $editProfileContainer = d.querySelector(".edit-profile-container"),
-        $pfps = d.querySelectorAll(".pfp-pic-container")
+        $pfps = d.querySelectorAll(".pfp-pic-container"),
+        $AllprofilePicCont = d.querySelectorAll(".pfp-pic-container"),
+        $pfpPreview = d.querySelector(".pfp-preview"),
+        $saveBtn = d.querySelector(".save-profile-changes"),
+        $userPfp = d.querySelector("#user-pfp")
 
+    for(let i = 0; i < $AllprofilePicCont.length; i++){
+        $AllprofilePicCont[i].setAttribute('data-pfp', i)
+    }
+    
+    const setPfp = (idImg)=>{
+        localStorage.setItem("pfp-id", idImg)
+        
+        let imgSrc = d.querySelector(`div[data-pfp="${idImg}"]`).firstElementChild.firstElementChild.getAttribute("src")
+        $pfpPreview.firstElementChild.src = imgSrc
+        $userPfp.src = imgSrc
+    }
+
+    let actualImg = 0
+
+    if(localStorage.getItem("pfp-id")){
+        if(localStorage.getItem("pfp-id") >= 0 && localStorage.getItem("pfp-id") < $AllprofilePicCont.length){
+            actualImg = localStorage.getItem("pfp-id")
+            console.log("hola")
+        }else{
+            localStorage.setItem("pfp-id", 0)
+            actualImg = 0
+        }
+    }
+
+    setPfp(actualImg)
 
     d.addEventListener("click", (e)=>{
-        console.log(e.target)
         if(e.target == $editPfpBtn || e.target == $editPgpBtnIcon){
             $editProfileContainer.classList.remove("hide-edit-profile")
         }
@@ -20,6 +48,15 @@ export function editProfile(){
         if(e.target.matches(".pfp-pic-container")){
             $pfps.forEach(el=> el.classList.remove("pfp-pic-selected"))
             e.target.classList.add("pfp-pic-selected")
+            
+            actualImg = e.target.getAttribute("data-pfp")
+
+            $pfpPreview.firstElementChild.src = e.target.firstElementChild.firstElementChild.getAttribute("src")
+        }
+        
+        if(e.target == $saveBtn){
+            setPfp(actualImg)
+            $editProfileContainer.classList.add("hide-edit-profile")
         }
 })
 }
