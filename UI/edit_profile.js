0c7a4c4e12cff029c70c055c6cba82563dd4e58f
Estarlin Valerio <valerioestarlin4tog@gmail.com>
2023-02-22 16:54:42 -0400

    pfps y points

--- UI/edit_profile.js
+++ UI/edit_profile.js
@@ -11,9 +11,39 @@ export function editProfile(){
         $userPfp = d.querySelector("#user-pfp"),
         $closeBtn = d.querySelector(".close-icon"),
         $inUseBox = d.createElement("div"),
-        $inUseBoxTxt = d.createElement("div")
+        $inUseBoxTxt = d.createElement("div"),
+        $lockedProtector = d.createElement("div")
         
+    $lockedProtector.classList.add("locked-pfp")
+    $lockedProtector.innerHTML = `<img src="img/icons/padlock.png" alt="">`
     
+    let markToNewPfp = 1000
+    const setLockedPfps = ()=>{
+        let unlockedByTheUser = Math.floor((parseInt((localStorage.getItem("HScore"))) || 0) / markToNewPfp)
+        console.log("hola", unlockedByTheUser)
+        let lockedPfpsCount = 6 - (unlockedByTheUser)
+        let availablePfps = $pfps.length - (lockedPfpsCount);
+
+        for(let i = 0; i < $pfps.length; i++){
+            if(i >= availablePfps){
+                let newLockOverlay = $lockedProtector.cloneNode(true)
+                $pfps[i].firstElementChild.appendChild(newLockOverlay)
+            }
+        }
+    }
+
+    const unlockPfps = ()=>{
+        let unlockedByTheUser = Math.floor((parseInt((localStorage.getItem("HScore"))) || 0) / markToNewPfp)
+        let lockedPfpsCount = 6 - (unlockedByTheUser)
+        let availablePfps = $pfps.length - (lockedPfpsCount);
+
+        for(let i = 1; i <= unlockedByTheUser; i++){
+            $pfps[availablePfps + i].firstElementChild.remove(newLockOverlay)
+        }
+    }
+
+    setLockedPfps()
+
     $inUseBoxTxt.classList.add("pfp-in-use-text")
     $inUseBoxTxt.innerHTML = "<p>In use</p>"
     $inUseBox.classList.add("pfp-in-use")
@@ -24,7 +54,7 @@ export function editProfile(){
     for(let i = 0; i < $AllprofilePicCont.length; i++){
         $AllprofilePicCont[i].setAttribute('data-pfp', i)
     }
-    
+
     const setPfp = (idImg)=>{
         localStorage.setItem("pfp-id", idImg)
         
@@ -46,26 +76,38 @@ export function editProfile(){
     }
 
     const setInUsePfp = ()=>{
-        let $selectedPfp = d.querySelector(`div[data-pfp="${actualImg}"]`)
-        $selectedPfp.classList.add("pfp-pic-selected")
+        let $selectedPfp = d.querySelector(`div[data-pfp="${localStorage.getItem("pfp-id") || 0}"]`)
+        $pfps.forEach(el=> el.classList.remove("pfp-in-use-border"))
         $selectedPfp.classList.add("pfp-in-use-border")
         $selectedPfp.firstElementChild.appendChild($inUseBox)
     }
+
+    const removeSelected = () =>{
+        $pfps.forEach(el=> el.classList.remove("pfp-pic-selected"))
+    }
+
+    const showPfpSelector = ()=>{
+        removeSelected()
+        $editProfileContainer.classList.add("hide-edit-profile")
+    } 
+
     setInUsePfp()
     setPfp(actualImg)
 
     d.addEventListener("click", (e)=>{
         if(e.target == $editPfpBtn || e.target == $editPgpBtnIcon){
             setInUsePfp()
+            unlockPfps()
             $editProfileContainer.classList.remove("hide-edit-profile")
         }
 
         if(e.target == $editProfileContainer){
+            removeSelected()
             $editProfileContainer.classList.add("hide-edit-profile")
         }
         
         if(e.target.matches(".pfp-pic-container")){
-            $pfps.forEach(el=> el.classList.remove("pfp-pic-selected"))
+            removeSelected()
             e.target.classList.add("pfp-pic-selected")
             
             actualImg = e.target.getAttribute("data-pfp")
@@ -75,11 +117,11 @@ export function editProfile(){
         
         if(e.target == $saveBtn){
             setPfp(actualImg)
-            $editProfileContainer.classList.add("hide-edit-profile")
+            showPfpSelector()
         }
 
         if(e.target == $closeBtn){
-            $editProfileContainer.classList.add("hide-edit-profile")
+            showPfpSelector()
         }
 })
 }
