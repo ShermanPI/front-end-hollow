7ac62c510ea33079c199c9e7992e2b17fe331f21
Sherman <shermanperez0@gmail.com>
2023-04-03 08:02:02 -0400

    logged out apparience

--- UI/load-user.js
+++ UI/load-user.js
@@ -1,3 +1,10 @@
 export function loadUser(){
-
+    fetch("http://127.0.0.1:5000/loginiu")
+    .then(res => res.ok? res.json() : res)
+    .then(userJson=>{
+        console.log("This user is still loged in: ", userJson)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 }
