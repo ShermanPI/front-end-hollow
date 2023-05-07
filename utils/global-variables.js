712273402137ab3b3cefa3f3b75140b184edef10
Sherman <shermanperez0@gmail.com>
2023-05-07 17:27:05 -0400

    URL api

--- utils/global-variables.js
+++ utils/global-variables.js
@@ -1,7 +1,7 @@
 export const globalVariables = Object.freeze({
     d: document,
     w: window,
-    apiURL: "http://127.0.0.1:5000",
+    apiURL: "https://back-end-hollow-knight-gduguifbia-uc.a.run.app",
     loginEndpoint: 'login',
     registerEndpoint: 'register',
     createCharacterEndPoint: 'characters',
